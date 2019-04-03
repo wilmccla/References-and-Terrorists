cd179e1f362f3572971472cf31e9eb0b2963aea5
William McClay <wilmccla@uat.edu>
2019-04-02 17:21:51 -0700

    Started Project

--- References and Terrorists/Source.cpp
+++ References and Terrorists/Source.cpp
@@ -19,6 +19,7 @@ int main()
 	cout << "\nWelcome Agent " + AgentName + " to the CIA's AntiHacker Program.\n";
 	cout << "We've gotten a tip that the bank account of a large celebrity has been targeted.\n";
 	cout << "Using this programs credentials, you should be able to access and check that bank account, as well as the suspected bank account the hackers moved the money to.";
+
 	system("pause");
 	
 }
