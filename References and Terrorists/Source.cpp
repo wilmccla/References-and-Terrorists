8ed3db8e1fcf2d033cdce2f77358d11225350959
William <wilmccla@uat.edu>
2019-04-02 21:01:26 -0700

    More work on project

--- References and Terrorists/Source.cpp
+++ References and Terrorists/Source.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 #include<string>
+#include<stdlib.h>
+#include<windows.h>
+#include <thread>
 
 using namespace std;
 
 //Declaring Functions
 void CheckNormalBankBalance();
 void CheckHackedBankBalance();
+void AgentChoice();
 
 //Declaring and Initializing Variables
-int OriginalBankBalance = 1000000;
-int HackedBankBalance = 1000;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+int HackersBackBalance = 100000000;
+int JoeCoffeShopBalance = 1000;
+
 string AgentName;
 
 int main()
@@ -17,19 +23,27 @@ int main()
 	cout << "Please enter your name:   ";
 	cin >> AgentName;
 	cout << "\nWelcome Agent " + AgentName + " to the CIA's AntiHacker Program.\n";
-	cout << "We've gotten a tip that the bank account of a large celebrity has been targeted.\n";
-	cout << "Using this programs credentials, you should be able to access and check that bank account, as well as the suspected bank account the hackers moved the money to.";
+	cout << "We've gotten a tip that a terrorist ring has amassed a large sum of money, and they plan to use it to establish a network of people here in America.\n";
+	cout << "Using this programs credentials, you should be able to access and check that bank account, as well as the the bank account of a 'Joe's Coffee Shop'.\n";
+	cout << "Joe's Coffee Shop has helped our mission countless times, and we're going to return the favor.";
+	
 
 	system("pause");
+	return 0;
+}
+
+void AgentChoice()
+{
 	
 }
 
+
 void CheckNormalBankBalance()
 {
-	cout << "$ " << OriginalBankBalance;
+	
 }
 
 void CheckHackedBankBalance()
 {
-	cout << "$ " << HackedBankBalance;
+	
 }
